a8aad5b1d31a2ab0e49ebfcf600f23d0c224979c
Carmen Agnese Santoro <s.carmen2101@gmail.com>
2020-12-04 19:26:36 +0100

    Creato una sorta di header, creata la base per la navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,54 @@
 <x-layout>
-Scrivo cose
+    
+    <header>
+        <div class="container h-100">
+            <div class="row align-items-center text-center h-100 pt-n5 header-align">
+                <div class="col-12 px-auto px-md-5">
+                    {{-- Barra di ricerca --}}
+                    <div class="row no-gutters mt-3 align-items-center justify-content-center">
+                        <div class="col-10 col-sm-11 col-md-9 col-lg-5">
+                            <input class="form-control form-control-lg form-searchBorder rounded-pill pr-5" type="search" placeholder="Cerca ciò che desideri">
+                        </div>
+                        <div class="col-auto">
+                            <button class="btn btn-searchColor border-0 rounded-pill ml-n5" type="button">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <main>
+        <div class="container-home">
+            <div class="row text-center">
+                <div class="col-6 col-md-3">
+                    Nome categoria
+                </div>
+                <div class="col-6 col-md-3">
+                    Nome categoria
+                </div>
+                <div class="col-6 col-md-3">
+                    Nome categoria
+                </div>
+                <div class="col-6 col-md-3">
+                    Nome categoria
+                </div>
+                <div class="col-6 col-md-3">
+                    Nome categoria
+                </div>
+                <div class="col-6 col-md-3">
+                    Nome categoria
+                </div>
+                <div class="col-6 col-md-3">
+                    Nome categoria
+                </div>
+                <div class="col-6 col-md-3">
+                    Nome categoria
+                </div>
+            </div>
+        </div>
+        
+    </main>
 </x-layout>
