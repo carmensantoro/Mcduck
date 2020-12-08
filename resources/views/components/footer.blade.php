48964592dc3d2fd3e8c6f1aa2cf1975b035db75a
Carmen Agnese Santoro <s.carmen2101@gmail.com>
2020-12-08 11:31:03 +0100

    Aggiunto il footer, che deve ancora essere sistemato, modificate le card delle categorie, aggiustata la navbar

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,56 @@
+<footer class="pt-5 mt-5 bg-dark">
+    <div class="row mx-auto">
+        
+        
+        {{-- Prima colonna --}}
+        <div class="col-12 col-md-6 col-xl-4">
+            <div class="col-12 col-sm-12 p-5">
+                <a href="index.html" class="d-flex align-items-center">
+                    <img src="/media/icon-coin.png" width="50px" alt="Logo">
+                    <div id="logo" class="h1 d-inline">Mcduck</div>
+                </a>
+                <div class="col-12 px-0">
+                    <p class="text-primary">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora nisi laudantium natus debitis reiciendis.</p>
+                </div>
+            </div>
+        </div>
+        
+        
+        {{-- Seconda colonna --}}
+        <div class="col-12 col-md-6 col-xl-4 py-5 mb-5 py-md-0 mb-md-0 align-self-center">
+            <div class="d-flex d-inline justify-content-center text-center">
+                <div class="mx-4 font-weight-bold"><a class="link-footer" href="">Link</a></div>
+                <div class="mx-4 font-weight-bold"><a class="link-footer" href="">Link</a></div>
+                <div class="mx-4 font-weight-bold"><a class="link-footer" href="">Link</a></div>
+                <div class="mx-4 font-weight-bold"><a class="link-footer" href="">Link</a></div>
+            </div>
+        </div>
+        
+        
+         {{-- Terza colonna --}}
+         <div class="col-12 col-xl-4 my-5 align-self-center">
+            <h5 class="text-center mt-n5 mb-4 text-primary">Social</h5>
+            <div class="d-flex justify-content-center">
+                <div class="social-icons">
+                    <a class="social-icon mx-1 mx-sm-2" target="_blank" href="#"><i class="fab fa-facebook-f text-white"></i></a>
+                    <a class="social-icon mx-1 mx-sm-2" target="_blank" href="#"><i class="fab fa-instagram text-white"></i></a>
+                    <a class="social-icon mx-1 mx-sm-2" target="_blank" href="#"><i class="fab fa-twitter text-white"></i></a>
+                    <a class="social-icon mx-1 mx-sm-2" href="#"><i class="fas fa-envelope text-white"></i></a>
+                </div>
+            </div>
+        </div>
+        
+        
+    </div>
+    
+    <div class="bg-secondary py-3">
+        <div class="container">
+            <div class="small text-center text-primary">
+                <a class="copyright-footer" href="{{route('home')}}">
+                    Copyright © 2020 - McDuck
+                </a>
+            </div>
+        </div>
+    </div>
+    
+</footer>
