17edc9fa29f4afe61cdb56d8d2737b3361b67c51
ivanpri <ivanpri14@gmail.com>
2020-12-09 19:17:51 +0100

    sistemato errore nella visualizzazione della navbar per colpa di 'is_revisor'

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -15,6 +15,10 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{route('ads.index', ['category'=>'all'])}}">Tutti gli annunci</a>
             </li>
+            @auth
+                
+            
+                
             
             @if(Auth::user()->is_revisor)
             <li class="nav-item">
@@ -26,6 +30,8 @@
                 </a>
             </li>
             @endif
+            @endauth
+
         </ul>
         
         
