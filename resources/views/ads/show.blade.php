aa56cbac183869d3d41c70aa9b7be623e6e23d7d
Carmen Agnese Santoro <s.carmen2101@gmail.com>
2020-12-10 19:02:05 +0100

    Salvate le immagini nei media e messe nel carosello che si trova nell'annuncio singolo. Cambiato lo stile del card-footer

--- resources/views/ads/show.blade.php
+++ resources/views/ads/show.blade.php
@@ -7,17 +7,17 @@
             
             <div class="col-12 col-md-6 position-relative">
                 <div class="product-img-detail w-100">
-                    <img src="https://www.mercedes-benz.it/passengercars/mercedes-benz-cars/models/amg-gt/coupe-c190/design/model-comparison/_jcr_content/comparisonslider/par/comparisonslide/exteriorImage.MQ6.12.2x.20200729130820.jpeg" class="img-fluid" alt="">
-                    <img src="https://www.mercedes-benz.it/passengercars/mercedes-benz-cars/models/amg-gt/coupe-c190/design/interior/_jcr_content/par/productinfotextimage_2094610276/media2/slides/videoimageslide/image.MQ6.7.2x.20190624120804.jpeg" class="img-fluid" alt="">
-                    <img src="https://www.mercedes-benz.it/passengercars/mercedes-benz-cars/models/amg-gt/coupe-c190/design/interior/_jcr_content/par/productinfotextimage/media2/slides/videoimageslide/image.MQ6.7.2x.20190624120804.jpeg" class="img-fluid" alt="">
-                    <img src="https://www.mercedes-benz.it/passengercars/mercedes-benz-cars/models/amg-gt/coupe-c190/design/exterior/_jcr_content/highlightcontainer/par/highlighttile.MQ6.0.2x.20190705085755.jpeg" class="img-fluid" alt="">
+                    <img src="/media/car1.jpeg" class="img-fluid" alt="">
+                    <img src="/media/car2.jpeg" class="img-fluid" alt="">
+                    <img src="/media/car3.jpeg" class="img-fluid" alt="">
+                    <img src="/media/car4.jpeg" class="img-fluid" alt="">
                 </div>
                 
                 <div class="product-img-nav pt-2">
-                    <img src="https://www.mercedes-benz.it/passengercars/mercedes-benz-cars/models/amg-gt/coupe-c190/design/model-comparison/_jcr_content/comparisonslider/par/comparisonslide/exteriorImage.MQ6.12.2x.20200729130820.jpeg" class="img-fluid mx-2" alt="">
-                    <img src="https://www.mercedes-benz.it/passengercars/mercedes-benz-cars/models/amg-gt/coupe-c190/design/interior/_jcr_content/par/productinfotextimage_2094610276/media2/slides/videoimageslide/image.MQ6.7.2x.20190624120804.jpeg" class="img-fluid mx-2" alt="">
-                    <img src="https://www.mercedes-benz.it/passengercars/mercedes-benz-cars/models/amg-gt/coupe-c190/design/interior/_jcr_content/par/productinfotextimage/media2/slides/videoimageslide/image.MQ6.7.2x.20190624120804.jpeg" class="img-fluid mx-2" alt="">
-                    <img src="https://www.mercedes-benz.it/passengercars/mercedes-benz-cars/models/amg-gt/coupe-c190/design/exterior/_jcr_content/highlightcontainer/par/highlighttile.MQ6.0.2x.20190705085755.jpeg" class="img-fluid mx-2" alt="">
+                    <img src="/media/car1.jpeg" class="img-fluid mx-2" alt="">
+                    <img src="/media/car2.jpeg" class="img-fluid mx-2" alt="">
+                    <img src="/media/car3.jpeg" class="img-fluid mx-2" alt="">
+                    <img src="/media/car4.jpeg" class="img-fluid mx-2" alt="">
                     
                 </div>
                 
