ec8ef4fe45b206451e30a56439f51948f182cd38
ivanpri <ivanpri14@gmail.com>
2020-12-14 20:00:28 +0100

    sistemata icona preferiti

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,12 +15,12 @@
                         <form action="{{route('ads.search')}}" method="get">
                             <div class="row no-gutters mt-3 align-items-center justify-content-center position-relative">
                                 <div class="col-3 coin-search position-absolute">
-                                <img src="/media/icon-coin.png" alt="" class="img-fluid">
+                                    <img src="/media/icon-coin.png" alt="" class="img-fluid">
                                 </div>
                                 <div class="col-12 d-flex p-2 pl-5 input-search rounded shadow">
                                     <input class="form-control form-control-lg rounded pr-5" id="text-input" type="text" name="q" placeholder="{{__('ui.search')}}">
-                          
-                          
+                                    
+                                    
                                     <button class="btn btn-searchColor border-0 rounded ml-n5" type="submit">
                                         <i class="text-white fa fa-search"></i>
                                     </button>
@@ -57,8 +57,19 @@
             <div class="container">
                 <div class="row">
                     @foreach ($ads as $ad)
-                    <div class="col-12">
-                        {{-- <a href="{{route('ads.show', compact('ad'))}}"> --}}
+                    <div class="col-12 position-relative">
+                        <div class="heart"> 
+                            <button class="btn" onclick="favor({{$ad->id}})">
+                                
+                                @if (($favorite->firstWhere('ad_id', "$ad->id")))
+                                <i id="{{$ad->id}}" class="fas fa-heart text-first fa-2x"></i>
+                                @else
+                                <i id="{{$ad->id}}" class="far fa-heart text-first fa-2x"></i>
+                                @endif
+                                
+                            </button>
+                        </div>
+                        <a href="{{route('ads.show', compact('ad'))}}">
                             <div class="card mb-3 overflow-hidden">
                                 <div class="row no-gutters">
                                     <div class="col-md-4">
@@ -73,39 +84,33 @@
                                             <p class="card-text"><small class="text-muted">{{$ad->category()->get()->implode('name', ' ')}}</small></p>
                                             <h3>{{$ad->price}} €</h3>
                                             <p class="card-text text-truncate">{{$ad->body}}</p>
-                                            <button class="btn" onclick="favor({{$ad->id}})"> 
-                                                @if (($favorite->firstWhere('ad_id', "$ad->id")))
-                                                    <i id="{{$ad->id}}" class="fas fa-heart"></i>
-                                                @else
-                                                    <i id="{{$ad->id}}" class="far fa-heart"></i>
-                                                @endif
-                                                
-                                            </button>
+                                            
                                             <p>Caricato da: {{$ad->user()->get()->implode('name', '')}}</p>            
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        {{-- </a> --}}
+                            </a>
+                        </div>
+                        @endforeach
                     </div>
-                    @endforeach
+                </div>
+            </section>
+            
+            
+            {{-- Annuncio per diventare revisore --}}
+            <div class="container-fluid bg-money my-5 p-5">
+                <div class="row text-center">
+                    <div class="col-12">
+                        <h2 class="font-weight-bold">Vuoi collaborare con noi e guadagnare soldi?</h2>
+                        <h6 class="font-weight-bold">Diventa nostro revisore!</h6>
+                        <a class="btn btn-custom rounded" href="{{ route('revisor.formRequest') }}">Clicca qui</a>
+                    </div>
                 </div>
             </div>
-        </section>
+            
+            
+        </main>
         
-        
-        {{-- Annuncio per diventare revisore --}}
-        <div class="container-fluid bg-money my-5 p-5">
-            <div class="row text-center">
-                <div class="col-12">
-                    <h2 class="font-weight-bold">Vuoi collaborare con noi e guadagnare soldi?</h2>
-                    <h6 class="font-weight-bold">Diventa nostro revisore!</h6>
-                    <a class="btn btn-custom rounded" href="{{ route('revisor.formRequest') }}">Clicca qui</a>
-                </div>
-            </div>
-        </div>
-        
-        
-    </main>
-
-</x-layout>
+    </x-layout>
+    
