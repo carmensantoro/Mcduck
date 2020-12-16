f48fc33e8830ed721e9bf4e04f7bd7cdc18c5fd8
Carmen Agnese Santoro <s.carmen2101@gmail.com>
2020-12-16 15:49:43 +0100

    Creata classe per le immagini delle card, che si trovano nella sezione 'tutti gli annunci'

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -27,7 +27,7 @@
                         <div class="row no-gutters">
                             <div class="col-md-4">
                                 @if(count($ad->images) > 0)
-                                <img src="{{$ad->images->first()->getUrl(400, 300)}}" class="card-img img-fluid" alt="..."> 
+                                <img src="{{$ad->images->first()->getUrl(400, 300)}}" class="card-img card-img-custom" alt="..."> 
                                 @endif
 
                             </div>
